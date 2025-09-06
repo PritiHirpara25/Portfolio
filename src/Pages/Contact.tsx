837f7a97475b8dbac0c234d28325bf83e85df9d5
PritiHirpara25 <pritihirpara256@gmail.com>
2025-09-06 14:21:16 +0530

    Refactor Contact and Portfolio components for improved structure and readability

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { FaMobileAlt } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
 
 export default function Contact() {
   return (
     <div className='px-52'>
-      Contact
+      <div className="flex flex-col items-center gap-4">
+        <h3 className="text-4xl">Contact Me</h3>
+        <p className="text-[#bcbcbc] text-lg text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+
+      <div className="flex justify-evenly py-20">
+        <div className="flex flex-col items-center">
+          <FaMobileAlt className="text-5xl" />
+          <span className="text-lg font-medium pt-3">Call Me</span>
+          <span className="text-[#bcbcbc] text-lg">7383412398</span>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <MdOutlineMail className="text-5xl" />
+          <span className="text-lg font-medium pt-3">Email Me</span>
+          <span className="text-[#bcbcbc] text-lg">pritihirpara256@gmail.com</span>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
 export default function Portfolio() {
   return (
     <div className='px-52'>
-      Portfolio
+      <div className="flex flex-col items-center gap-4">
+        <h3 className="text-4xl">My Works</h3>
+        <p className="text-[#bcbcbc] text-lg text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
     </div>
   )
 }
